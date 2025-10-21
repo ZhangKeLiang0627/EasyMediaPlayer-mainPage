c0fac370f4a18ea4f882b5c3ab875e2f12a16898
ZhangKeLiang0627 <111654293+ZhangKeLiang0627@users.noreply.github.com>
2025-10-21 23:23:17 +0800

    [update] 增加文件目录增加、删除和移动的事件监控

--- main.cpp
+++ main.cpp
@@ -2,7 +2,21 @@
 #include "Model.h"
 #include "udiskMonitor.h"
 
+#include "utils/xepoll/xinotify.h"
+
 static Page::Model *model;
+
+Xinotify inotify_;
+
+void NotifyFileHandler()
+{
+    log_debug("[xinotify] /mnt/UDISK/test.json file is changed");
+}
+
+void NotifyDirHandler()
+{
+    log_info("[xinotify] /mnt/UDISK dir is changed");
+}
 
 static void exitCallback(void);
 
@@ -13,18 +27,32 @@ int main(int argc, char *argv[])
 
     log_info("[Sys] eMP_mainPage begin!");
 
+    std::string testPath = "/mnt/UDISK/test.json";
+    int ret = inotify_.AddFileWatch(testPath, std::bind(&NotifyFileHandler));
+    if (ret != true)
+    {
+        log_error("[xinotify] error, can not create inotify!");
+    }
+
+    std::string testDirPath = "/mnt/UDISK";
+    ret = inotify_.AddDirWatch(testDirPath, std::bind(&NotifyDirHandler));
+    if (ret != true)
+    {
+        log_error("[xinotify] error, can not create inotify!");
+    }
+
     // Init HAL
     HAL::Init();
 
     // model初始化
     model = new Page::Model(exitCallback);
 
-    // udisk设备监控启动
-    int ret = UDiskMonitor::getInstance().start(HAL::onUDiskEvent);
-    if (ret != 0)
-    {
-        std::cerr << "Failed to start UDiskMonitor, error code: " << ret << std::endl;
-    }
+    // // udisk设备监控启动
+    // int ret = UDiskMonitor::getInstance().start(HAL::onUDiskEvent);
+    // if (ret != 0)
+    // {
+    //     std::cerr << "Failed to start UDiskMonitor, error code: " << ret << std::endl;
+    // }
 
     for (;;)
     {
--- utils/xepoll/xinotify.h
+++ utils/xepoll/xinotify.h
@@ -13,12 +13,15 @@ public:
 
     int HandleEvent();
     bool AddFileWatch(const std::string &path, std::function<void()> handler);
+    bool AddDirWatch(const std::string &path, std::function<void(const std::string &)> handler);
     bool DelFileWatch(const std::string &path);
+    bool DelDirWatch(const std::string &path);
 
 private:
     int inotify_fd_;
-    std::unordered_map<std::string, int> file_watch_fd_map_;
+    std::unordered_map<std::string, int> watch_fd_map_;
     std::unordered_map<int, std::function<void()>> listeners_file_change_;
+    std::unordered_map<int, std::pair<std::string, std::function<void(const std::string &)>>> listeners_dir_change_;
 };
 
 #endif
