1700cbc2b050dc3acb0996e6a2c3f7a0ba61e8a3
kkl <1184665829@qq.com>
2025-12-15 15:54:04 +0800

    [delete] 去掉udisk监控

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include "../inc/common_inc.h"
 #include "Model.h"
-#include "udiskMonitor.h"
 
 static Page::Model *model;
-static UDiskMonitor monitor;
 
 static void exitCallback(void);
 
@@ -19,13 +17,6 @@ int main(int argc, char *argv[])
 
     // model初始化
     model = new Page::Model(exitCallback);
-
-    // udisk设备监控启动
-    int ret = monitor.start(HAL::onUDiskEvent);
-    if (ret != 0)
-    {
-        std::cerr << "Failed to start UDiskMonitor, error code: " << ret << std::endl;
-    }
 
     for (;;)
     {
